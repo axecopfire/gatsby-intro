ff7ee9f7be715563750c2d688411e50ebbcb1e3e
axecopfire <sbraun990@gmail.com>
2019-06-19 16:14:01 -0400

    init commit

---
+++ notes.md
@@ -0,0 +1,10 @@
+# Jason Lengstorf's Gatsby Intro
+
+- [Addy Osmani's PRPL Pattern](https://developers.google.com/web/fundamentals/performance/prpl-pattern/)
+- Gastby uses React's Reach Router
+- babel Javascript vscode extension for syntax highlighting
+- NavLink, activeClassName
+
+- Loaded Dependencies
+    - react-helmet
+    - emotion
---
+++ src/hooks/use-posts.js
@@ -0,0 +1,28 @@
+import { graphql, useStaticQuery } from 'gatsby';
+
+const usePosts = () => {
+    const data = useStaticQuery(graphql`
+    query MyQuery {
+        __typename
+        allMdx {
+          nodes {
+            excerpt
+            frontmatter {
+              title
+              slug
+              author
+            }
+          }
+        }
+      }
+    `)
+
+    return data.allMdx.nodes.map(post => ({
+            title: post.frontmatter.title,
+            author: post.frontmatter.author,
+            slug: post.frontmatter.slug,
+            excerpt: post.excerpt
+        })); 
+};
+
+export default usePosts;
